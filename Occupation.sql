77d536ca7640cb95a768cd606fb133f99cad89ae
Esraa Fareha <112290483+EsraaFareha@users.noreply.github.com>
2023-03-11 13:04:48 +0200

    Delete Occupation.sql

--- Occupation.sql
+++
@@ -1,5 +0,0 @@
-select  Doctor, Professor, Singer, Actor
-from (select row_number() over(partition by occupation order by name) numb ,* from OCCUPATIONS ) as source
-pivot
-(min(name) for occupation in  (Doctor, Professor, Singer, Actor)) as pivotable
-order by numb
